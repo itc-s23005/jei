39ee6a60ccccb56059e43a1643f9a5a73e99c884
itc-s23005 <s23005@std.it-college.ac.jp>
2024-05-24 10:43:26 +0900

    第４８回　java 課題

---
+++ d48Q22.java
@@ -0,0 +1,12 @@
+enum Connection {OFFLINE, ONLINE}
+
+class d48Q22 {
+		public static void main(String[] args) {
+				Connection s = Connection.OFFLINE;
+				if (s == Connection.OFFLINE) {
+						s = Connection.OFFLINE;
+				}
+				System.out.println(s);
+		}
+}
+
