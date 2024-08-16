dd31b73b90336ac78a72ef4449393bd4c62f1d93
itc-s23005 <s23005@std.it-college.ac.jp>
2024-08-16 17:23:52 +0900

    課題の提出

--- d52/d52Q27kadai.java
+++ d52/d52Q27kadai.java
@@ -1,17 +1,20 @@
-//s23005
+// s23005
 
 enum Signal {
-				RED, YELLOW, GREEN;
+    RED, YELLOW, GREEN;
 }
+
 public class d52Q27kadai {
-				public static void main(String[] args) {
-								Signal sig = Signal.YELLOW;
-								if (sig == Signal.GREEN) {
-												System.out.println("緑");
-								} else if (sig == Signal.YELLOW) {
-												System.out.println("黃");
-								} else {
-												System.out.println("赤");
-								}
-				}
+    public static void main(String[] args) {
+        Signal sig = Signal.YELLOW;
+        
+        if (sig == Signal.GREEN) {
+            System.out.println("緑");
+        } else if (sig == Signal.YELLOW) {
+            System.out.println("黄");
+        } else {
+            System.out.println("赤");
+        }
+    }
 }
+
--- d52/d52Q2kadai.java
+++ d52/d52Q2kadai.java
@@ -1,19 +1,22 @@
-//s23005
-//
-//
-//
+// s23005
+
 class Temper {
-				private double temperature;
-				public double getTemperature() { return temperature; }
-				public void setTemperature(double temperature) {
-								this.temperature = temperature;
-				}
+    private double temperature;
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
 }
+
 public class d52Q2kadai {
-				public static void main(String[] args) {
-								Temper t = new Temper();
-								t.setTemperature(17.2);
-								System.out.println(t.getTemperature());
-				}
+    public static void main(String[] args) {
+        Temper t = new Temper();
+        t.setTemperature(17.2);
+        System.out.println(t.getTemperature());
+    }
 }
-								
+
